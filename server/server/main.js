4290a73f199856908e5acea90a5bb3cecf426742
Aryan Goharzad <arrygoo@gmail.com>
2017-05-22 14:50:00 -0400

    Lint

--- server/server/main.js
+++ server/server/main.js
@@ -1,9 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
+import {Meteor} from 'meteor/meteor';
+import {Mongo} from 'meteor/mongo';
 
 const Phones = new Mongo.Collection('phones');
 
 Meteor.startup(() => {
-  Phones.upsert({ phoneName: '1' }, { position: 0, phoneName: '1' });
   // code to run on server at startup
 });
