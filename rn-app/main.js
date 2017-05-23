38f9c219ef9fd0369131a7ba20cc957d5218a9b0
Aryan Goharzad <arrygoo@gmail.com>
2017-05-22 23:19:03 -0400

    Lots of updates on mobile

--- rn-app/main.js
+++ rn-app/main.js
@@ -2,196 +2,262 @@ import React from 'react';
 import Expo, { Sound, Audio, Accelerometer, KeepAwake, Constants } from 'expo';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Meteor, { createContainer } from 'react-native-meteor';
-Meteor.connect('ws://192.168.1.3:3000/websocket'); //do this only once
+import generateName from 'sillyname';
+import _ from 'lodash';
+Meteor.connect('ws://192.168.1.4:3000/websocket'); //do this only once
+
+const noteToDegree = noteString => {
+	switch (noteString) {
+		case 'G':
+			return '75';
+			break;
+		case 'F':
+			return '45';
+			break;
+		case 'E':
+			return '0';
+			break;
+		case 'D':
+			return '-45';
+			break;
+		case 'C':
+			return '-75';
+			break;
+		default:
+			return '0';
+	}
+};
 
 class AccelerometerSensor extends React.Component {
-  state = {
-    color: '100',
-    accelerometerData: {}
-  };
+	state = {
+		color: '100',
+		accelerometerData: {},
+		note: 'A4',
+		angle: 0,
+		name: '----'
+	};
 
-  componentDidMount() {
-    Accelerometer.setUpdateInterval(300);
-    this._toggle();
-    this.registerPhone()
-  }
+	getAppInfo() {
+		return Meteor.collection('appInfo').findOne({});
+	}
+	componentDidMount() {
+		Accelerometer.setUpdateInterval(300);
+		this._toggle();
+		this.registerPhone();
+	}
 
-  registerPhone() {
-    console.log("Registering phone", Constants.sessionId);
-    Meteor.collection('phones').insert({ sessionId: Constants.sessionId })
-  }
+	registerPhone() {
+		console.log('Registering phone', Constants.sessionId);
+		const name = generateName();
+		this.setState({ name });
+		Meteor.collection('phones').insert({
+			sessionId: Constants.sessionId,
+			name
+		});
+	}
 
-  componentWillUnmount() {
-    this._unsubscribe();
-  }
+	componentWillUnmount() {
+		this._unsubscribe();
+	}
 
-  _toggle = () => {
-    if (this._subscription) {
-      this._unsubscribe();
-    } else {
-      this._subscribe();
-    }
-  };
+	getMyNextNote() {
+		if (!this.getAppInfo()) return;
+		const turn = this.getAppInfo().turn;
+		const playingArray = this.getAppInfo().playingArray;
+		const foundItem = _.find(playingArray, item => {
+			return item.sessionId === Constants.sessionId && item.turn >= turn;
+		});
+		console.log('==', turn);
+		return (foundItem && foundItem.note) || 'N/A';
+	}
 
-  _slow = () => {
-    Accelerometer.setUpdateInterval(300);
-    // const res = Meteor.collection('phones').findOne({ phoneName: '1' });
-    // console.log('===', res);
-  };
+	getMyNextTurn() {
+		if (!this.getAppInfo() || !this.getAppInfo().turn) return;
+		const turn = this.getAppInfo().turn;
+		const playingArray = this.getAppInfo().playingArray;
+		const foundItem = _.find(playingArray, item => {
+			return item.sessionId === Constants.sessionId && item.turn >= turn;
+		});
+		console.log('==', turn);
+		return (foundItem && foundItem.turn) || 'N/A';
+	}
 
-  _fast = () => {
-    Accelerometer.setUpdateInterval(500);
-  };
+	_toggle = () => {
+		if (this._subscription) {
+			this._unsubscribe();
+		} else {
+			this._subscribe();
+		}
+	};
 
-  _subscribe = () => {
-    this._subscription = Accelerometer.addListener(result => {
-      // this._handlePlaySoundAsync(result.y * 3)
+	_slow = () => {
+		Accelerometer.setUpdateInterval(300);
+		// const res = Meteor.collection('phones').findOne({ phoneName: '1' });
+		// console.log('===', res);
+	};
 
-      styles.sensor.backgroundColor = '#1dd';
-      this.setState({ accelerometerData: result });
-      const me = Meteor.collection('phones').findOne({ sessionId: Constants.sessionId });
-      const lastUpdated = new Date();
-      if (me) {
-        Meteor.collection('phones').update(
-          me._id,
-          {
-            accelerometerData: result,
-            phoneName: '1',
-            sessionId: Constants.sessionId,
-            lastUpdated
-          },
-          // (cberror, cbresult) => {
-          //   console.log('err:', xcberror, ' cbresult', cbresult);
-          // }
-        );
-      } else {
-        console.error("couldnt find me");
-      }
-    });
-  };
+	_fast = () => {
+		Accelerometer.setUpdateInterval(500);
+	};
 
-  _unsubscribe = () => {
-    this._subscription && this._subscription.remove();
-    this._subscription = null;
-  };
+	_subscribe = () => {
+		this._subscription = Accelerometer.addListener(accelerometerData => {
+			if (Constants.platform && Constants.platform.ios) {
+				accelerometerData.y *= -1;
+			}
 
-  render() {
-    let { x, y, z } = this.state.accelerometerData;
-    let colorx = round(x * 100) + 101;
-    let colory = round(y * 100) + 101;
-    let colorz = round(z * 100) + 101;
+			styles.sensor.backgroundColor = '#1dd';
 
-    const angle = Math.round(y * 90);
-    let note = '-';
-    if (angle > 54) {
-      note = 'G4';
-    } else if (angle > 18) {
-      note = 'F4';
-    } else if (angle > -18) {
-      note = 'E4';
-    } else if (angle > -54) {
-      note = 'D4';
-    } else {
-      note = 'C4';
-    }
+			// Calculate Angle and Note
+			const angle = Math.round(accelerometerData.y * 90);
+			let note = 'B';
+			if (angle > 54) {
+				note = 'G';
+			} else if (angle > 18) {
+				note = 'F';
+			} else if (angle > -18) {
+				note = 'E';
+			} else if (angle > -54) {
+				note = 'D';
+			} else {
+				note = 'C';
+			}
 
-    let colorStyle = StyleSheet.create({
-      colors: { backgroundColor: `rgb(${colorx},${colory},${colorz})` }
-    });
-    return (
-      <View style={colorStyle.colors}>
-        <Text>Accelerometer:</Text>
-        <Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
-        <KeepAwake />
+			// Set accelerometerData and angle
+			this.setState({ accelerometerData, angle, note });
 
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={this._toggle} style={styles.button}>
-            <Text>Toggle</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={this._slow}
-            style={[styles.button, styles.middleButton]}
-          >
-            <Text>Slow</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this._fast} style={styles.button}>
-            <Text>Fast</Text>
-          </TouchableOpacity>
-        </View>
-        <Text sytle={colorStyle.colors}> {angle} </Text>
-        <Text sytle={colorStyle.colors}> {note} </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-        <Text sytle={colorStyle.colors}> </Text>
-      </View>
-    );
-  }
+			// Update database
+			const me = Meteor.collection('phones').findOne({
+				sessionId: Constants.sessionId
+			});
+			const lastUpdated = new Date();
+			if (me) {
+				Meteor.collection('phones').update(
+					me._id,
+					{
+						$set: {
+							accelerometerData,
+							sessionId: Constants.sessionId,
+							angle,
+							note,
+							lastUpdated
+						}
+					}
+					// (cberror, cbresult) => {
+					//   console.log('err:', xcberror, ' cbresult', cbresult);
+					// }
+				);
+			} else {
+				console.error('couldnt find me');
+			}
+		});
+	};
+
+	_unsubscribe = () => {
+		this._subscription && this._subscription.remove();
+		this._subscription = null;
+	};
+
+	isCorrectNote = () => {
+		return this.getMyNextNote() === this.state.note;
+	};
+
+	render() {
+		let { x, y, z } = this.state.accelerometerData;
+		// let colorx = round(x * 100) + 101;
+		// let colory = round(y * 100) + 101;
+		// let colorz = round(z * 100) + 101;
+		let backgroundColor = (this.isCorrectNote() && 'green') || 'red';
+
+		let colorStyle = StyleSheet.create({
+			// colors: { backgroundColor: `rgb(${colorx},${colory},${colorz})` },
+			colors: { backgroundColor },
+			largeText: { fontSize: 46 }
+		});
+		return (
+			<View style={colorStyle.colors}>
+				{/* <Text>Accelerometer: {JSON.stringify()}</Text> */}
+				<Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
+				<KeepAwake />
+
+				<View style={styles.buttonContainer}>
+					<TouchableOpacity onPress={this._toggle} style={styles.button}>
+						<Text>Toggle</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						onPress={this._slow}
+						style={[styles.button, styles.middleButton]}
+					>
+						<Text>Slow</Text>
+					</TouchableOpacity>
+					<TouchableOpacity onPress={this._fast} style={styles.button}>
+						<Text>Fast</Text>
+					</TouchableOpacity>
+				</View>
+				<Text style={colorStyle.largeText}> 📐: {this.state.angle}° </Text>
+				<Text style={colorStyle.largeText}>
+					{/* {(this.isCorrectNote() && '✅') || '⛔'}: {this.state.note} */}
+					{(this.isCorrectNote() && '      ✅') || '      ⛔'}
+				</Text>
+				<Text style={colorStyle.largeText}>
+					🎵:
+					{/* {this.getMyNextNote()} */}
+					{/* {' - '} */}
+					{noteToDegree(this.getMyNextNote())}
+				</Text>
+				<Text style={colorStyle.largeText}> 👨‍🌾: {this.state.name} </Text>
+				<Text style={colorStyle.largeText}>
+					🎼: {this.getAppInfo() && this.getAppInfo().turn}
+				</Text>
+				<Text style={colorStyle.largeText}>
+					{' '}
+					🥅: {this.getMyNextTurn()}
+				</Text>
+			</View>
+		);
+	}
 }
 
 function round(n) {
-  if (!n) {
-    return 0;
-  }
+	if (!n) {
+		return 0;
+	}
 
-  return Math.floor(n * 100) / 100;
+	return Math.floor(n * 100) / 100;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    marginTop: 15
-  },
-  button: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#eee',
-    padding: 10
-  },
-  middleButton: {
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderColor: '#ccc'
-  },
-  sensor: {
-    marginTop: 15,
-    paddingHorizontal: 10
-  }
+	container: {
+		flex: 1
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		alignItems: 'stretch',
+		marginTop: 15
+	},
+	button: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#eee',
+		padding: 10
+	},
+	middleButton: {
+		borderLeftWidth: 1,
+		borderRightWidth: 1,
+		borderColor: '#ccc'
+	},
+	sensor: {
+		marginTop: 15,
+		paddingHorizontal: 10
+	}
 });
 
 Expo.registerRootComponent(
-  createContainer(params => {
-    return {
-      phones: Meteor.collection('phones').find()
-    };
-  }, AccelerometerSensor)
+	createContainer(params => {
+		return {
+			phones: Meteor.collection('phones').find(),
+			appInfo: Meteor.collection('appInfo').find()
+		};
+	}, AccelerometerSensor)
 );
